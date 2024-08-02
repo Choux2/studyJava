7dd4f98c40154a89791fbd8cf6521a21f16fb34f
choux2 <dulfla0307@naver.com>
2024-08-02 17:52:37 +0900

    Initial commit

---
+++ java/src/study0802/test57.java
@@ -0,0 +1,16 @@
+package study0802;
+
+public class test57 {
+    public static void numbering (int limit) {
+        int i = 0;               //매개변수(parameter) - 입력값을 수용하기 위한 변수가 정의되어 있는 부분
+        while (i<limit) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        numbering(5); //인자(argument)
+        numbering(3);
+    }
+}
