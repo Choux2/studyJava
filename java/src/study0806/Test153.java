589eda1b13fdd5074afd6e9c3e34f326adb6086e
choux2 <dulfla0307@naver.com>
2024-08-07 01:37:41 +0900

    Collections framework - List/Set

--- java/src/study0806/Test153.java
+++ java/src/study0806/Test153.java
@@ -21,8 +21,8 @@ public class Test153 {
             System.out.println(hi.next());
         }
 
-        //hasNext 반복 할 데이터가 있으면 true, 더 이상 반복할 데이터가 없다면 false 리턴
-        //next hasNext가 true라는 것은 next가 리턴할 데이터가 존재한다는 의미
+        //.hasNext: 반복할 데이터가 있으면 true, 더 이상 반복할 데이터가 없다면 false 리턴
+        //.next: hasNext가 true라는 것은 next가 리턴할 데이터가 존재한다는 의미
 
         ArrayList<Integer> B = new ArrayList<>();
         B.add(1);
