7df5a9110f773fee5ddbc87caa78c6be3dc98537
choux2 <dulfla0307@naver.com>
2024-08-06 23:55:11 +0900

    Collections framework - List/Set

---
+++ java/src/study0806/Test153.java
@@ -0,0 +1,40 @@
+package study0806;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Test153 {
+    //List 내가 입력한 모든 값 저장
+    //Set 중복값은 제외하고 고유한 값들만 저장
+    public static void main(String[] args) {
+        HashSet<Integer> A = new HashSet<Integer>();
+        A.add(1);
+        A.add(2);
+        A.add(2);
+        A.add(2);
+        A.add(2);
+        A.add(3);
+
+        Iterator hi = (Iterator) A.iterator();
+        while (hi.hasNext()) {
+            System.out.println(hi.next());
+        }
+
+        //hasNext 반복 할 데이터가 있으면 true, 더 이상 반복할 데이터가 없다면 false 리턴
+        //next hasNext가 true라는 것은 next가 리턴할 데이터가 존재한다는 의미
+
+        ArrayList<Integer> B = new ArrayList<>();
+        B.add(1);
+        B.add(2);
+        B.add(2);
+        B.add(2);
+        B.add(2);
+        B.add(3);
+
+        Iterator ai = (Iterator) B.iterator();
+        while (ai.hasNext()) {
+            System.out.println(ai.next());
+        }
+    }
+}
